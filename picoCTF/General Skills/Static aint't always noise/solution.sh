1c147ecbf74ec0e79d2990bfc79d7f9878d5e4e6
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-09-20 21:33:05 +0200

    Create solution.sh

---
+++ picoCTF/General Skills/Static aint't always noise/solution.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+chmod +x static
+chmod +x ltdis.sh
+
+ltdis.sh static
+
+if [[ $? == 0 ]]; then
+  cat static.ltdis.strings.txt | grep picoCTF
+else
+  strings static | grep picoCTF
+fi
