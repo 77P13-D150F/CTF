2b69b15937d4892c2e92a261237c68ebfe55d9ea
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-09-22 16:39:33 +0200

    Create solution.sh

---
+++ picoCTF/General Skills/Tab, Tab, Attack/solution.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+unzip Addadshashanammu.zip
+
+file Addadshashanammu/Almurbalarammi/Ashalmimilkala/Assurnabitashpi/Maelkashishi/Onnissiralis/Ularradallaku/fang-of-haynekhtnamet
+
+strings Addadshashanammu/Almurbalarammi/Ashalmimilkala/Assurnabitashpi/Maelkashishi/Onnissiralis/Ularradallaku/fang-of-haynekhtnamet
