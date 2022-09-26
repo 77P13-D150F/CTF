9b2c20060c22db949ee4dcba37e51d8c91cf9ce0
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-09-26 11:46:42 +0200

    Update solution.py

--- picoCTF/General Skills/convertme.py/solution.py
+++ picoCTF/General Skills/convertme.py/solution.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-# I pasted the encrypted flag from the convertme.py file. The secret key is also in the file
+# I pasted the encrypted flag from the convertme.py file. The secret key is also in the file.
+# You can also run the python script and look up online for a binary conversion of the random integer it displays.
+
 key = 'enkidu'
 flag_enc = chr(0x15) + chr(0x07) + chr(0x08) + chr(0x06) + chr(0x27) + chr(0x21) + chr(0x23) + chr(0x15) + chr(0x5f) + chr(0x05) + chr(0x08) + chr(0x2a) + chr(0x1c) + chr(0x5e) + chr(0x1e) + chr(0x1b) + chr(0x3b) + chr(0x17) + chr(0x51) + chr(0x5b) + chr(0x58) + chr(0x5c) + chr(0x3b) + chr(0x42) + chr(0x57) + chr(0x5c) + chr(0x0d) + chr(0x5f) + chr(0x06) + chr(0x46) + chr(0x5c) + chr(0x13)
 
