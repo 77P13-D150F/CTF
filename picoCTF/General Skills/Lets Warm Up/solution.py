a0cd08087d19bf2ceb3f107c98ca3b15754e25dc
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-09-27 08:20:57 +0200

    Update solution.py

--- picoCTF/General Skills/Lets Warm Up/solution.py
+++ picoCTF/General Skills/Lets Warm Up/solution.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env pyhon3
 
-import codecs
+#import codecs
 
 #flag = str(codecs.decode('0x70'[2:], 'hex'), 'utf-8')
 flag = chr(int('0x70', 16))
 
-print('picoCTF{' + flag + '}')
+print(''.join('picoCTF{', flag, '}'))
