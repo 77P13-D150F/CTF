e2a93254b0ac997860e956ac3488e37da4318913
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-09-27 14:30:25 +0200

    Create solution.sh

---
+++ picoCTF/General Skills/Based/solution.sh
@@ -0,0 +1,8 @@
+ #!/usr/bin/bash
+ 
+echo 'you catch this flag using an online ascii decoder, here an example:'
+echo 'https://www.dcode.fr/ascii-code'
+
+nc jupiter.challenges.picoctf.org 29956
+ 
+exit 0
