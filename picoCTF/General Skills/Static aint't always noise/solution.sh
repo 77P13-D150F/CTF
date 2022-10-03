0f2f157b1e21e52d5788871a8bb540073e51e031
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-03 22:18:53 +0200

    Update solution.sh

--- picoCTF/General Skills/Static aint't always noise/solution.sh
+++ picoCTF/General Skills/Static aint't always noise/solution.sh
@@ -3,13 +3,9 @@
 #chmod +x static
 chmod +x ltdis.sh
 
-# one liner
-# ltdis.sh static || strings static | grep picoCTF
-
-# extended
 ltdis.sh static
 
-if [[ $? == 0 ]]; then
+if [ $? == 0 ]; then
   cat static.ltdis.strings.txt | grep picoCTF
 else
   strings static | grep picoCTF
