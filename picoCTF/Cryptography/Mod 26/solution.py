348a9e8cb2e876164b938550cd9d004cf8a25143
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-05 21:48:17 +0200

    Update solution.py

--- picoCTF/Cryptography/Mod 26/solution.py
+++ picoCTF/Cryptography/Mod 26/solution.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 
-enc = 'cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_nSkgmDJE}'
+import string
+from collections import deque
+
+# To obtain the flag apply ROT13 to the string cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_nSkgmDJE}
+enc = 'cvpbPGS{arkg_gvzr_V\'yy_gel_2_ebhaqf_bs_ebg13_nSkgmDJE}'
+
+def encrypt(rot, text):
+    az_deque = deque([l for l in string.ascii_lowercase])                            
+    az_deque.rotate(rot)
+    new_alphabet = ''.join(az_deque)
+                                                                  
+    rot_tab = text.maketrans(string.ascii_lowercase, new_alphabet)                   
+    cipher = text.translate(rot_tab)                                                 
+    
+    AZ_deque = deque(string.ascii_uppercase)                                         
+    AZ_deque.rotate(rot)
+    new_alphabet = ''.join(AZ_deque)
+    
+    rot_tab = cipher.maketrans(string.ascii_uppercase, new_alphabet)
+    return cipher.translate(rot_tab)
+
+
+print(encrypt(13, enc))
