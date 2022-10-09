1042fe8d29db4dbbca0b9ca8ba4b1312a062e1c9
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-09 21:51:15 +0200

    Update solution.py

--- picoCTF/Cryptography/Mind your Ps and Qs/solution.py
+++ picoCTF/Cryptography/Mind your Ps and Qs/solution.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
-# python3 -m pip install pycryptodome
+# pip install pycryptodome
 from Crypto.Util.number import inverse, long_to_bytes
+#from Cryptodome.Util.number import inverse, long_to_bytes
 
-# python3 -m pip install primefac
-from primefac import primefac
+# pip install primefac
+from primefac import primegen
+
+from tqdm import tqdm
+
+def decrypt(p, q):
+    phi = (p - 1) * (q - 1)
+    d = inverse(e, phi)
+    m = pow(c, d, n)
+    print(long_to_bytes(m))
 
 with open('values', 'r') as file:
   enc = file.read().split('\n')
@@ -15,9 +24,7 @@ e = enc[3][3:]
 
 # if the modulo n is too small, it can be factorzed to obtain the primes p and q.
 # time of processing can be very long on low end computers.
-(p, q) = primefac(n)
-phi = (p - 1) * (q - 1)
-d = inverse(e, phi)
-m = pow(c, d, n)
-
-print(long_to_bytes(m))
+primes = primegen(limit=n//2)
+for i in tqdm(primes):
+    if n % i == 0:
+        decrypt(i, n//i) 
