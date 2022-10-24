9a8520ec518362278be297552b4caea5b07fdd7f
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-24 13:39:16 +0200

    Update solution.py

--- picoCTF/Cryptography/Mini RSA/solution.py
+++ picoCTF/Cryptography/Mini RSA/solution.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 with open('cipertext', 'r') as file:
-  ciphertext = file.read().split('\n')
+  enc_flag = file.read().split('\n')
   
-n = int(ciphertext[0][ciphertext[0].index(':')+1])
-e = int(ciphertext[1][ciphertext[1].index(':')+1])
-c = int(ciphertext[2][ciphertext[2].index(':')+1])
+n = int(enc_flag[0][enc_flag[0].index(':')+2:])
+e = int(enc_flag[1][enc_flag[1].index(':')+2:])
+c = int(enc_flag[3][enc_flag[3].index(':')+2:])
