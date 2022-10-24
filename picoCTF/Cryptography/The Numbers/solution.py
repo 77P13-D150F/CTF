0019e9f86f8b8e15b9e5b3347b1f32575cef2e39
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-24 11:11:00 +0200

    Update solution.py

--- picoCTF/Cryptography/The Numbers/solution.py
+++ picoCTF/Cryptography/The Numbers/solution.py
@@ -1,1 +1,4 @@
 #!/usr/bin/env python3
+
+c = [16, 9, 3, 15, 3, 20, 6, '{', 20, 8, 5, 14, 21, 13, 2, 5, 18, 19, 13, 1, 19, 15, 14, '}']
+print(''.join([chr(i + ord('a')-1) if type(i) == int else i for i in c]))
