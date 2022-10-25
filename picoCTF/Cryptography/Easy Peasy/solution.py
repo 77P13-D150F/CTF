463573d018855b4de52c4e75d0bd6e0c0e0c2288
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-25 08:13:19 +0200

    Update solution.py

--- picoCTF/Cryptography/Easy Peasy/solution.py
+++ picoCTF/Cryptography/Easy Peasy/solution.py
@@ -16,18 +16,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # At begin of the script, the flag is encrypted consuming certain amount of key bytes.
         flag_length = len(bytes.fromhex(flag))
         
-        # To reset the key to the begin we need to encrypt a string of null bytes to use all the remaining key length.
+        # To reset the key to the begin we need to encrypt a string of null bytes (but could be any type of data) to use all the remaining key length.
         # The remaining key length is obtained by subtracting total key length with the flag length in bytes.
         s.sendall(b''.join([(KEY_LENGTH - flag_length) * b'\x00', b'\n']))
         buffer = s.recv(1024)
         while b'?' not in buffer:
                 buffer = s.recv(1024)
         
-        # The key used to encrypt the flag can be obtained now by sending null bytes of the lenght of the flag.
+        # From otp.py we know that the flag is encrypted with one XOR operator.
+        # So with the key position now in reset, we send null bytes of the lenght of the flag to obtain the key.
         s.sendall(b''.join([flag_length * b'\x00', b'\n']))
         key = s.recv(1024).decode().split('\n')[1]
 
-        # The flag is obained by xor operation. We know it by reading the otp.py file
+        # We finally decrypt the flag by one XOR operation.
         flag = ''.join([str(xor(int(flag[i], 16), int(key[i], 16))) for i in range(len(flag))])
 
         print(''.join(['picoCTF{', bytes.fromhex(flag).decode(), '}']))
