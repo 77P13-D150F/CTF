ee9c726c804cf136847a1c9086f39107a0b60dd7
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-25 12:05:15 +0200

    Update solution.py

--- picoCTF/Cryptography/Easy1/solution.py
+++ picoCTF/Cryptography/Easy1/solution.py
@@ -4,3 +4,5 @@ enc_flag = 'UFJKXQZQUNB'
 key = 'SOLVECRYPTO'
 
 # From the file table.txt we see that this is a Vigenere cipher
+flag = ''.join([chr(((ord(c) - ord(key[i % len(key)]) - 2 * ord('A')) % 26) + ord('A')) for i, c in enumerate(enc_flag)])
+print(''.join(['picoCTF{', flag, '}']))
