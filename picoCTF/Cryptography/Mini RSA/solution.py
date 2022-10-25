d55705bfdf92421826ebf94d3b0d1b497e33c714
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-25 08:16:56 +0200

    Update solution.py

--- picoCTF/Cryptography/Mini RSA/solution.py
+++ picoCTF/Cryptography/Mini RSA/solution.py
@@ -4,8 +4,6 @@ from Crypto.Util.number import long_to_bytes
 import gmpy2
 
 
-MAX_PAD = 10000
-
 with open(r'H:\04_Python\ciphertext', 'r') as file:
     enc_flag = file.read().split('\n')
 
@@ -14,8 +12,11 @@ e = int(enc_flag[1][enc_flag[1].index(':')+2:])
 c = int(enc_flag[3][enc_flag[3].index(':')+2:])
 
 
-# The plaintext is padded so that (m ** e) is just barely larger than n.
-# If pow(m, e) % n = c, then pow(m, e) = pad * n + c, for some pad so that m is a true root of pow(pad * n + c, e).
+# The challenge says that the plaintext is padded so that (m ** e) is just barely larger than n.
+# So if pow(m, e) % (n * pad) = c, then pow(m, e) = pad * n + c, then we can obtain the flag by
+# screening through a pad range until m is a true root of pow(pad * n + c, e).
+
+MAX_PAD = 10000
 
 for pad in range(MAX_PAD):
     print(f'\rPadding {pad} of {MAX_PAD}', end='\r', flush=True)
