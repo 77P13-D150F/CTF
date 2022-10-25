c5e50c75e7a71a6ed2d14e2e29962c31b6f0acfb
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-25 12:12:51 +0200

    Update solution.py

--- picoCTF/Cryptography/caesar/solution.py
+++ picoCTF/Cryptography/caesar/solution.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+import string
+from collections import deque
+
+
+def encrypt(rot, text):
+    az_deque = deque([l for l in string.ascii_lowercase])                            
+    az_deque.rotate(rot)
+    new_alphabet = ''.join(az_deque)
+                                                                  
+    rot_tab = text.maketrans(string.ascii_lowercase, new_alphabet)                   
+    cipher = text.translate(rot_tab)                                                 
+    
+    AZ_deque = deque(string.ascii_uppercase)                                         
+    AZ_deque.rotate(rot)
+    new_alphabet = ''.join(AZ_deque)
+    
+    rot_tab = cipher.maketrans(string.ascii_uppercase, new_alphabet)
+    return cipher.translate(rot_tab)
+
+
+print(encrypt(13, enc_flag))
