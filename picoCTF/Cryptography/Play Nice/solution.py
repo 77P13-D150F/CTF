049d858d7f36425bdd9229276b6e5bd5b9cde312
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-10-25 17:24:51 +0200

    Update solution.py

--- picoCTF/Cryptography/Play Nice/solution.py
+++ picoCTF/Cryptography/Play Nice/solution.py
@@ -7,3 +7,6 @@ server = ('mercury.picoctf.net', 19354)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(server)
+        data = s.recv(1024).decode().split('\n')
+        alphabet = data[0][22:]
+        message = data[1][31:]
