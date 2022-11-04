4cfd7f8daa2d337861c1c83e64b8596537000d20
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-11-04 16:41:09 +0100

    Create solution.py

---
+++ picoCTF/Cryptography/Vigenere/solution.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+# The challenge title implies an encryption by Vigenere cipher.
+# We also know that the flag is encrypted using the key "CYLAB".
+
+import string
+
+
+with open('cipher.txt', 'r') as file:
+    enc_flag = file.read()
+
+key = 'CYLAB'
+key = [ord(i) - ord('A') for i in key]
+
+print(enc_flag)
+flag = ''
+i = 0
+for c in enc_flag:
+    if c.islower():
+        flag += chr(((ord(c) - ord('a') - key[i % len(key)]) % 26) + ord('a'))
+        i += 1
+    elif c.isupper():
+        flag += chr(((ord(c) - ord('A') - key[i % len(key)]) % 26) + ord('A'))
+        i += 1
+    else:
+        flag += c
+print(flag)
